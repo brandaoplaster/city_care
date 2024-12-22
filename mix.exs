31d13f40c43905b3290d5f4cac0b3b5002a28f3c
brandaoplaster <brandaoplaster@gmail.com>
2024-12-22 18:26:46 -0300

    chore: Installing excoveralls, sobelow and credo libs

--- mix.exs
+++ mix.exs
@@ -58,7 +58,11 @@ defmodule CityCare.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.18.0", only: :test},
+      {:sobelow, "~> 0.13.0", only: :dev}
     ]
   end
 
